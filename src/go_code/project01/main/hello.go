a4bbd508725d99ce43ef08c4fb0a34fda8249eed
huzejun <huzejun1990>
2022-05-02 17:44:58 +0800

    hello.go 最初 开始练习 2022年5月2日17:44:50

---
+++ src/go_code/project01/main/hello.go
@@ -0,0 +1,17 @@
+// 要求开发一个hello.go 程序，可以输出 “hello,world;"
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("hello,world!") // shift_alt+向下箭头
+	// fmt.Println("hello,world!") // shift_alt+向下箭头
+	// fmt.Println("hello,world!") // shift_alt+向下箭头
+
+	// var num = 10;
+	fmt.Println("hello")
+
+	//定义一个变量,如果你没有使用这个变量，就会编译无法通过
+	var num = 10
+
+}
