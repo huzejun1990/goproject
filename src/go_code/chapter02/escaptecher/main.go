f85b61a194ccf3869884dfd011db01786e63b6c1
huzejun <huzejun1990>
2022-05-05 03:00:33 +0800

    定义变量，2022年5月5日03:00:19

--- src/go_code/chapter02/escaptecher/main.go
+++ src/go_code/chapter02/escaptecher/main.go
@@ -4,13 +4,13 @@ import "fmt" //fmt 包中提供格式化，输出，输入的函数
 func main() {
 
 	fmt.Println("tom\tjack")
-
 	fmt.Println("hello\nworld")
-
 	fmt.Println("F:\\360Downloads\\go系列视频\\视频-1（更新）\\视频")
-
 	fmt.Println("tom说\"i like you\"")
 
 	// \r 回车，从当前行的最前面开始输出，覆盖掉以前的内容
 	fmt.Println("天龙八部雪山飞狐\r张三厉害")
+
+	fmt.Println("helloworldhelloworldhelloworld\n", "helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld\n", "helloworldhelloworldhelloworldhelloworldhelloworld\n")
+	//var num = 2 + 4 * 5
 }
